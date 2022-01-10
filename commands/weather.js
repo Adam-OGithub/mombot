3a947657a8ddee5623df02165281215226ca9da8
Adam-OGithub <adamoverton2012@gmail.com>
2022-01-09 21:36:21 -0600

    updated weather cmd

--- commands/weather.js
+++ commands/weather.js
@@ -18,15 +18,39 @@ exports.run = async (client, message, args, discord) => {
     }
 
     weather.getAllWeather(function (err, loc) {
+      const weatherDes = loc.weather[0].description;
+      const m = loc.main;
+      const temp = m.temp;
+      const feelsLike = m.feels_like;
+      const minTemp = m.temp_min;
+      const maxTemp = m.temp_max;
+      const humidity = m.humidity;
+      const visibility = loc.visibility / 1000;
+      let emote = ``;
+      if (temp <= 40) {
+        emote = `cold_face`;
+      } else if (temp > 40 <= 55) {
+        emote = `cold_sweat`;
+      } else if (temp > 55 && temp <= 75) {
+        emote = `smiley`;
+      } else if (temp > 75 && temp <= 85) {
+        emote = `upside_down`;
+      } else if (temp > 85) {
+        emote = `hot_face`;
+      } else {
+        emote = `face_with_monocle`;
+      }
       let embed = new Discord.MessageEmbed()
-        .setTitle(`Weather for ${loc.name}`)
+        .setTitle(`Weather for ${loc.name} - ${weatherDes} :${emote}:`)
         .setColor(config.color)
         .setDescription(
-          `Temperature is ${round(loc.main.temp)}ºF and feels like ${round(
-            loc.main.feels_like
-          )}ºF\n Min temp ${round(loc.main.temp_min)}ºF, Max Temp ${round(
-            loc.main.temp_max
-          )}ºF \n Humidity is ${round(loc.main.humidity)}`
+          `Temperature is ${round(temp)}ºF and feels like ${round(
+            feelsLike
+          )}ºF\n Min temp ${round(minTemp)}ºF, Max Temp ${round(
+            maxTemp
+          )}ºF \n Humidity is ${round(humidity)} with visibility at ${round(
+            visibility
+          )} Miles`
         );
       message.channel.send(embed);
     });
