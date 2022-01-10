0f133c43989421b7ab01cb70de8386cbe0c6abfb
Adam-OGithub <adamoverton2012@gmail.com>
2022-01-09 22:49:03 -0600

    added markov in utils

--- custom_nodemods/utils.js
+++ custom_nodemods/utils.js
@@ -3,5 +3,32 @@ const randomWord = (arr) => arr[Math.trunc(Math.random() * arr.length)];
 
 const round = (myInt) => Math.trunc(myInt);
 
+function markovMe(input) {
+  const markovChain = {};
+  const textArr = input.split(" ");
+  for (let i = 0; i < textArr.length; i++) {
+    let word = textArr[i].toLowerCase().replace(/[\W_]/, "");
+    if (!markovChain[word]) {
+      markovChain[word] = [];
+    }
+    if (textArr[i + 1]) {
+      markovChain[word].push(textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
+    }
+  }
+  const words = Object.keys(markovChain);
+  let word = words[Math.floor(Math.random() * words.length)];
+  let result = "";
+  for (let i = 0; i < words.length; i++) {
+    result += word + " ";
+    let newWord =
+      markovChain[word][Math.floor(Math.random() * markovChain[word].length)];
+    word = newWord;
+    if (!word || !markovChain.hasOwnProperty(word))
+      word = words[Math.floor(Math.random() * words.length)];
+  }
+  return result;
+}
+
 exports.randomWord = randomWord;
 exports.round = round;
+exports.markovChain = markovMe;
