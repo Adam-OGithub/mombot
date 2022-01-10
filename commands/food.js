0763ecd6a0c07ad19fc8449073f4fa15f5f91ec0
Adam-OGithub <adamoverton2012@gmail.com>
2022-01-09 21:09:32 -0600

    updated food.js to include image and put all info in description field

--- commands/food.js
+++ commands/food.js
@@ -33,53 +33,17 @@ exports.run = async (client, message, args, discord) => {
       for (let i = 0; i < ingredients.length; i++) {
         str += `${ingredients[i]}: ${amount[i]}\n`;
       }
-      const out = `${mealObj.strMeal} - ${mealObj.strArea} - ${mealObj.strCategory}`;
-      const apple = `${mealObj.strInstructions}`;
-      console.log(`-==========`);
-      console.log(out.length);
-      console.log(out);
-      console.log(`-==========`);
-      console.log(apple.length);
-      console.log(apple);
-      console.log(`-==========`);
-      console.log(str.length);
-      console.log(str);
-      console.log(`-==========`);
       let embed = new Discord.MessageEmbed()
         .setTitle(
           `${mealObj.strMeal} - ${mealObj.strArea} - ${mealObj.strCategory}`
         )
+        .setURL(mealObj.strSource)
+        .setImage(mealObj.strMealThumb)
         .setColor(config.color)
-        .setDescription(`${mealObj.strInstructions}`);
-      const sL = str.length;
-      if (sL <= 256) {
-        embed.addField(str);
-      } else if (sL > 256 && sL <= 512) {
-        embed.addField(`${str.slice(0, sL / 2)}`);
-        embed.addField(`${str.slice(sL / 2 + 1, sL)}`);
-      } else if (sL > 512 && sL <= 768) {
-        embed.addField(`${str.slice(0, sL / 3)}`);
-        embed.addField(`${str.slice(sL / 3 + 1, (sL / 3) * 2)}`);
-        embed.addField(`${str.slice((sL / 3) * 2 + 1, sL)}`);
-      } else {
-        embed.addField(`${str.slice(0, sL / 8)}`);
-        embed.addField(`${str.slice(sL / 8, (sL / 8) * 2)}`);
-        embed.addField(`${str.slice((sL / 8) * 2 + 1, (sL / 8) * 3)}`);
-        embed.addField(`${str.slice((sL / 8) * 3 + 1, (sL / 8) * 4)}`);
-        embed.addField(`${str.slice((sL / 8) * 4 + 1, (sL / 8) * 5)}`);
-        embed.addField(`${str.slice((sL / 8) * 5 + 1, (sL / 8) * 6)}`);
-        embed.addField(`${str.slice((sL / 8) * 6 + 1, (sL / 8) * 7)}`);
-        embed.addField(`${str.slice((sL / 8) * 7, sL)}`);
-        //greate than 1024
-      }
+        .setDescription(
+          `${mealObj.strInstructions} \n\n__Ingredients__\n ${str}`
+        );
 
-      if (str.length > 2048) {
-        embed = `WOW that is a long recipe better pick another one`;
-      }
-
-      embed.fields.forEach((obj) => {
-        obj.value = "___________________________";
-      });
       message.channel.send(embed);
     });
 };
