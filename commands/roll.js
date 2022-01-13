f5365386aba0cdc857454e5c5d9d9d0d28eda1cc
Adam-OGithub <adamoverton2012@gmail.com>
2022-01-13 10:38:25 -0600

    updated dice output for total and added bigint to prevent overflow when adding die

--- commands/roll.js
+++ commands/roll.js
@@ -6,21 +6,48 @@ const {
   makeEmbed,
 } = require("../custom_nodemods/utils.js");
 const config = require("../config.json");
+
+const roll = (die) => randomInt(0, die);
+const addVals = (pre, cur) => BigInt(pre) + BigInt(cur);
+const total = (allDice) => allDice.reduce(addVals);
+const cycleRoll = (die, multi, notSingle = false, overTen) => {
+  let str = ``;
+  if (notSingle) {
+    const diceCount = [];
+    for (let i = 0; i < +multi; i++) {
+      let dRoll = roll(die);
+      if (i <= 10) {
+        str += `Rolled: ${dRoll} \n`;
+      }
+      diceCount.push(dRoll);
+    }
+    if (overTen) {
+      str += `Added other dice to total ...`;
+    }
+    str += `\nTotal: ${total(diceCount)}`;
+  } else {
+    str = roll(die);
+  }
+  return str;
+};
+
 exports.run = async (client, msg, args, discord, infoObj) => {
   if (args[0] !== undefined) {
     const lower = infoObj.msg.toLowerCase().split(" ").join("").split("d");
     const multi = lower[0].split(`${config.prefix}roll`)[1];
     const die = Number.parseInt(lower[1], 10);
+    const multiDie = Number.parseInt(multi, 10);
     if (typeof die === "number" && die <= 90071992547409) {
       let str = ``;
-      if (+multi > 0) {
-        for (let i = 0; i < +multi; i++) {
-          str += `Rolled ${randomInt(0, die)} \n`;
-        }
+      if (multiDie > 0 && multiDie <= 10) {
+        str = cycleRoll(die, multi, true);
+      } else if (multiDie > 10) {
+        str = cycleRoll(die, multi, true, true);
       } else {
-        str = ` ${randomInt(0, die)} `;
+        str = cycleRoll(die);
       }
-      const embed = makeEmbed(`Dice Roll!`, str);
+      const t = (overOne) => (overOne > 0 ? `s!` : `!`);
+      const embed = makeEmbed(`Dice Roll${t(multi)}`, str);
       sMsg(msg.channel, embed);
     } else if (die > 90071992547409) {
       sMsg(
