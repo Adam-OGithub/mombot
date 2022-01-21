0b7fa307246815e0167c89b784b7450eecf0d66f
Adam-OGithub <adamoverton2012@gmail.com>
2022-01-20 21:43:03 -0600

    updated mom with more sayings from jardin

--- commands/mom.js
+++ commands/mom.js
@@ -14,8 +14,9 @@ const {
   markovChain,
   capFirst,
   sMsg,
+  randomInt,
 } = require("../custom_nodemods/utils.js");
-
+const axios = require("../node_modules/axios");
 //Replaces all words with random word that meet criteria
 const loopVars = (sArr) => {
   sArr.forEach((word, i) => {
@@ -50,30 +51,42 @@ const loopVars = (sArr) => {
 
 let sentArr = [];
 exports.run = async (client, msg, args, discord) => {
+  const ranInt = randomInt(0, 10);
   let loop = true;
   let i = 0;
   let str = ``;
-  //loops until a unique sentence is found if no unique sentence is found uses markov chain
-  while (loop) {
-    let select = randomWord(momSayings);
-    let sArr = select.split(" ");
-    str = loopVars(sArr);
-    if (sentArr.includes(str)) {
-      //increase i
-      i++;
-    } else if (i > 75) {
-      //tried 20 times just send it
-      const mark = markovChain(sentArr.join(" "));
-      const ran = randomWord(mark.split(".").join("").split("!"));
-      const str = `${capFirst(ran)}.`;
-      sMsg(msg.channel, str);
-      sentArr = [];
-      loop = false;
-    } else {
-      //not in list
-      sentArr.push(str);
-      sMsg(msg.channel, str);
-      loop = false;
+  if (ranInt < 6) {
+    //loops until a unique sentence is found if no unique sentence is found uses markov chain
+    while (loop) {
+      let select = randomWord(momSayings);
+      let sArr = select.split(" ");
+      str = loopVars(sArr);
+      if (sentArr.includes(str)) {
+        //increase i
+        i++;
+      } else if (i > 75) {
+        //tried 20 times just send it
+        const mark = markovChain(sentArr.join(" "));
+        const ran = randomWord(mark.split(".").join("").split("!"));
+        const str = `${capFirst(ran)}.`;
+        sMsg(msg.channel, str);
+        sentArr = [];
+        loop = false;
+      } else {
+        //not in list
+        sentArr.push(str);
+        sMsg(msg.channel, str);
+        loop = false;
+      }
     }
+  } else {
+    axios.get("http://www.madsci.org/cgi-bin/lynn/jardin/SCG").then((res) => {
+      const resArr = res.data.split("\n");
+      const content = resArr.slice(
+        resArr.indexOf(`<h2>`) + 2,
+        resArr.indexOf(`</h2>`)
+      )[0];
+      sMsg(msg.channel, content);
+    });
   }
 };
