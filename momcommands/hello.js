9e2c88fe68620d9dd076e29b4e5ba386d747bc13
Adam-OGithub <adamoverton2012@gmail.com>
2022-02-18 13:23:33 -0600

    updated hello remove sql

--- momcommands/hello.js
+++ momcommands/hello.js
@@ -7,43 +7,6 @@ exports.run = async (client, message, args, discord, infoObj) => {
   let query;
   const guildIds = [];
   const channelsObj = [];
-  //myReq.query = `SELECT helloid FROM hello WHERE guildid = "${infoObj.guildID}"`;
-
-  // axios.post(config.web.dburl, myReq).then((res) => {
-  //   let out;
-  //   if (res?.data?.result !== undefined) {
-  //     out = res?.data?.result[0]?.helloid;
-  //   }
-  //   if (
-  //     out !== null &&
-  //     out !== undefined &&
-  //     out === infoObj.channelId &&
-  //     message.author.bot !== true
-  //   ) {
-  //     //if query returns channelid is same as hello
-  //     for (const [key, value] of client.guilds.cache) {
-  //       guildIds.push(client.guilds.cache.get(key));
-  //     }
-  //     myReq.query = `SELECT * FROM hello`;
-  //     axios.post(config.web.dburl, myReq).then((res) => {
-  //       const results = res?.data?.result;
-  //       if (results !== undefined) {
-  //         results.forEach((result, i) => {
-  //           channelsObj.push(guildIds[i].channels.cache.get(result.helloid));
-  //         });
-  //         channelsObj.forEach((channelObj) => {
-  //           if (channelObj?.id && channelObj?.id !== infoObj?.channelId) {
-  //             const embeded = makeEmbed(
-  //               `Hello "${infoObj?.guildName}"- "${message.author.tag}"`,
-  //               ` ${infoObj?.msg} `
-  //             );
-  //             sMsg(channelObj, embeded);
-  //           }
-  //         });
-  //       }
-  //     });
-  //   }
-  // });
   mongoQuery({ guildId: infoObj.guildID }, "hello").then((res) => {
     if (res.length === 0) {
       //do nothing as channel does not exist for hello
