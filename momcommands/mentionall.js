72855a575bb7d37b9d93bb7e0904d691b036da41
Adam-OGithub <adamoverton2012@gmail.com>
2022-02-18 13:45:54 -0600

    fixed mentionall

--- momcommands/mentionall.js
+++ momcommands/mentionall.js
@@ -1,32 +1,25 @@
 "use strict";
 const config = require("../config.json");
 const { sMsg, getChannel } = require("../custom_nodemods/utils.js");
+const { mongoQuery } = require("../custom_nodemods/mongoCon.js");
 const axios = require("../node_modules/axios");
 exports.run = async (client, message, args, discord, infoObj) => {
-  const myReq = {};
-  myReq.query = `SELECT prisonid FROM prison WHERE guildid = "${infoObj.guildID}"`;
-  axios
-    .post(config.web.dburl, myReq)
-    .then((res) => {
-      const out = res.data.result[0]?.prisonid;
+  mongoQuery({ guildId: infoObj.guildID }, "prison").then((res) => {
+    if (res.length === 0) {
+      sMsg(
+        message.channel,
+        `Sweety Pie <@${message.author.id}> I am your mother,I brought you into this world and I can take you out of it!`
+      );
+    } else {
+      sMsg(
+        message.channel,
+        `Sweety Pie <@${message.author.id}> I am your mother,I brought you into this world and I can take you out of it!`
+      );
 
-      if (out !== undefined && out !== null) {
-        sMsg(
-          message.channel,
-          `Sweety Pie <@${message.author.id}> I am your mother,I brought you into this world and I can take you out of it!`
-        );
-        sMsg(
-          getChannel(out, infoObj),
-          `<@${message.author.id}> you belong here`
-        );
-      } else {
-        sMsg(
-          message.channel,
-          `Sweety Pie <@${message.author.id}> I am your mother,I brought you into this world and I can take you out of it!`
-        );
-      }
-    })
-    .catch((e) => {
-      console.log(`${e}`);
-    });
+      sMsg(
+        getChannel(res[0].prisonId, infoObj),
+        `<@${message.author.id}> you belong here`
+      );
+    }
+  });
 };
