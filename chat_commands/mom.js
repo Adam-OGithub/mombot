234a6d2f472d83c6a614aed8b93d59ccdf0b8ead
Adam-OGithub <adamoverton2012@gmail.com>
2023-04-21 22:16:41 -0500

    added a fix for incomplete sentences

--- chat_commands/mom.js
+++ chat_commands/mom.js
@@ -5,6 +5,7 @@ const {
   sendChannelMsgNoInteraction,
   getChannelFromClient,
 } = require('../custom_node_modules/utils');
+const endings = ['.', '?', '!'];
 
 const configuration = new Configuration({
   apiKey: config.openAi.key,
@@ -18,9 +19,42 @@ const startChat = async (client, fullMsg, chatMsg) => {
     prompt: chatMsg,
   });
   const chatResponse = completion.data.choices[0].text;
+  console.log('CHAT RES:', chatResponse);
+  let finalResponse = '';
+  let theEnd = '';
+  if (
+    chatResponse.endsWith('.') ||
+    chatResponse.endsWith('?') ||
+    chatResponse.endsWith('!')
+  ) {
+    finalResponse = chatResponse;
+  } else {
+    let currentIndex = 0;
+    // const newChatResponse = chatResponse.replace(/\n/gi, '');
+    console.log('NEW CHAT:', chatResponse);
+    const splitSentence = chatResponse.split('');
+    console.log('SPLITSENTENCE:', splitSentence);
+    splitSentence.forEach((letter, i) => {
+      console.log('letter:', letter);
+      if (endings.includes(letter)) {
+        currentIndex = i;
+      }
+    });
+    console.log('INDEX', currentIndex);
+    let newIndex = currentIndex;
+
+    if (currentIndex === 0) {
+      newIndex = splitSentence.length;
+      theEnd = '.';
+    }
+
+    finalResponse = splitSentence.slice(0, newIndex + 1).join('');
+  }
+
+  console.log('FINALRES:', finalResponse);
   sendChannelMsgNoInteraction(
     getChannelFromClient(client, fullMsg.guildId, fullMsg.channelId),
-    chatResponse,
+    finalResponse + theEnd,
     false
   );
 };
