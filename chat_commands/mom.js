52fa133e92b2de2456a9fd2af7a34618eb3a8e0d
Adam-OGithub <adamoverton2012@gmail.com>
2023-04-21 22:17:57 -0500

    added a fix for incomplete sentences

--- chat_commands/mom.js
+++ chat_commands/mom.js
@@ -19,7 +19,6 @@ const startChat = async (client, fullMsg, chatMsg) => {
     prompt: chatMsg,
   });
   const chatResponse = completion.data.choices[0].text;
-  console.log('CHAT RES:', chatResponse);
   let finalResponse = '';
   let theEnd = '';
   if (
@@ -31,16 +30,12 @@ const startChat = async (client, fullMsg, chatMsg) => {
   } else {
     let currentIndex = 0;
     // const newChatResponse = chatResponse.replace(/\n/gi, '');
-    console.log('NEW CHAT:', chatResponse);
     const splitSentence = chatResponse.split('');
-    console.log('SPLITSENTENCE:', splitSentence);
     splitSentence.forEach((letter, i) => {
-      console.log('letter:', letter);
       if (endings.includes(letter)) {
         currentIndex = i;
       }
     });
-    console.log('INDEX', currentIndex);
     let newIndex = currentIndex;
 
     if (currentIndex === 0) {
@@ -51,7 +46,6 @@ const startChat = async (client, fullMsg, chatMsg) => {
     finalResponse = splitSentence.slice(0, newIndex + 1).join('');
   }
 
-  console.log('FINALRES:', finalResponse);
   sendChannelMsgNoInteraction(
     getChannelFromClient(client, fullMsg.guildId, fullMsg.channelId),
     finalResponse + theEnd,
